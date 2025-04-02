b9c52fac184641f763db482c14e3ca06784945ef
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-04-02 16:37:17 +0600

    Alhamdulillah, Added Cover Image

--- tocScript.js
+++ tocScript.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Select the TOC container
+    let coverDiv = document.querySelector("#cover");
+    coverDiv.style.breakBefore = 'page';
     let tocDiv = document.querySelector(".toc");
+    tocDiv.style.breakBefore = 'page';
     // Select all h2 and h3 elements for ToC generation
     let headings = document.querySelectorAll("h2, h3");
 
